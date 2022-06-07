d65005456b5d90a306b718051e66beac0070d857
sbx_user1051 <sbx_user1051@169.254.60.101>
2022-06-07 18:51:28 +0000

    Tue Jun 07 18:51:28 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,5 +1,3 @@
-
-
 # noinspection PyUnusedLocal
 # skus = unicode string
 
@@ -8,105 +6,113 @@ from signal import ITIMER_REAL
 
 
 def sanityCheck(skus):
-    #empty basket
+    # empty basket
     if not skus:
         return 0
-    #malformed token
+    # malformed token
     if not skus.isupper():
         return -1
 
+
 def collateItemAmounts(skus, single_items):
-    item_amounts={}
+    item_amounts = {}
 
     for item in skus:
-        #Not in the basket
+        # Not in the basket
         if not item in single_items:
             return -1
 
-        #Add item to the item amounts, or increment
+        # Add item to the item amounts, or increment
         if item in item_amounts:
-            item_amounts[item]=item_amounts[item]+1
+            item_amounts[item] = item_amounts[item] + 1
         else:
-            item_amounts[item]=1
+            item_amounts[item] = 1
     return item_amounts
 
+
 def applySpecialOfferA(item_amounts):
-    addition=0
-    if item_amounts["A"]>=5:
-        #How many times does the special offer fit?
-        offer_amount=(item_amounts["A"]-item_amounts["A"]%5)/5
-        #Remove this many items from the item amounts, and add
-        #sum to basket:
-        addition+=offer_amount*200
-        item_amounts["A"]=item_amounts["A"]-offer_amount*5
+    addition = 0
+    if item_amounts["A"] >= 5:
+        # How many times does the special offer fit?
+        offer_amount = (item_amounts["A"] - item_amounts["A"] % 5) / 5
+        # Remove this many items from the item amounts, and add
+        # sum to basket:
+        addition += offer_amount * 200
+        item_amounts["A"] = item_amounts["A"] - offer_amount * 5
 
-
-    if item_amounts["A"]>=3:
-        #How many times does the special offer fit?
-        offer_amount=(item_amounts["A"]-item_amounts["A"]%3)/3
-        #Remove this many items from the item amounts, and add
-        #sum to basket:
-        addition+=offer_amount*130
-        item_amounts["A"]=item_amounts["A"]-offer_amount*3
+    if item_amounts["A"] >= 3:
+        # How many times does the special offer fit?
+        offer_amount = (item_amounts["A"] - item_amounts["A"] % 3) / 3
+        # Remove this many items from the item amounts, and add
+        # sum to basket:
+        addition += offer_amount * 130
+        item_amounts["A"] = item_amounts["A"] - offer_amount * 3
     return addition
+
 
 def applySpecialOfferB(item_amounts):
-    addition=0
-    if item_amounts["B"]>=2:
-        #How many times does the special offer fit?
-        offer_amount=(item_amounts["B"]-item_amounts["B"]%2)/2
-        #Remove this many items from the item amounts, and add
-        #sum to basket:
-        addition+=offer_amount*45
-        item_amounts["B"]=item_amounts["B"]-offer_amount*2
+    addition = 0
+    if item_amounts["B"] >= 2:
+        # How many times does the special offer fit?
+        offer_amount = (item_amounts["B"] - item_amounts["B"] % 2) / 2
+        # Remove this many items from the item amounts, and add
+        # sum to basket:
+        addition += offer_amount * 45
+        item_amounts["B"] = item_amounts["B"] - offer_amount * 2
     return addition
 
+
+def applySpecialOfferE(item_amounts):
+    # How this will work is that
+    # For each pair of E's, we can remove one B, as long as there are B's to remove
+    if item_amounts["B"] >= 2:
+        # How many times does the special offer fit?
+        offer_amount = (item_amounts["B"] - item_amounts["B"] % 2) / 2
+        # Remove this many items from the item amounts, and add
+        # sum to basket:
+        addition += offer_amount * 45
+        item_amounts["B"] = item_amounts["B"] - offer_amount * 2
+    return addition
 
 
 def checkout(skus):
-    #First, we sum all possible amounts of items. Then, if
-    #A special offer for an item exists, we try to fit that first. If not, then go with the normal price table
-    #Writing tests to test_sum.py in order to stay on top of things
-    #Write tests first, and adapt a TDD-approach
+    # First, we sum all possible amounts of items. Then, if
+    # A special offer for an item exists, we try to fit that first. If not, then go with the normal price table
+    # Writing tests to test_sum.py in order to stay on top of things
+    # Write tests first, and adapt a TDD-approach
 
+    # Assume that each basket is a string of characters, each
+    # an individual item: for instance, "AABBCC"
 
-    #Assume that each basket is a string of characters, each 
-    #an individual item: for instance, "AABBCC"
+    # Formulate a hashmap for single items, and another for special offers
+    single_items = {"A": 50, "B": 30, "C": 20, "D": 15}
 
+    # We do all non-special offer tests first.
+    # Testing works now. Now to start with the first cases: iterate all the items, and
+    # Add their prices into the basket.
+    # If we find an item that is not in basket, return -1
 
-    #Formulate a hashmap for single items, and another for special offers
-    single_items={"A":50,"B":30,"C":20,"D":15}
+    # Keep running tests after every change when reformatting
 
-    #We do all non-special offer tests first.
-    #Testing works now. Now to start with the first cases: iterate all the items, and
-    #Add their prices into the basket.
-    #If we find an item that is not in basket, return -1
-
-    #Keep running tests after every change when reformatting
-
-    item_amounts=collateItemAmounts(skus,single_items)
-    basket=0
-    #First, we try to fit the special offers in:
-    #If the amount of items is geq 3, recude
+    item_amounts = collateItemAmounts(skus, single_items)
+    basket = 0
+    # First, we try to fit the special offers in:
+    if "E" in item_amounts:
+        basket += applySpecialOfferE(item_amounts)
+    # If the amount of items is geq 3, recude
     if "A" in item_amounts:
-        basket+=applySpecialOfferA(item_amounts)
-    #Same with the other one:
+        basket += applySpecialOfferA(item_amounts)
+    # Same with the other one:
     if "B" in item_amounts:
-        basket+=applySpecialOfferB(item_amounts)
+        basket += applySpecialOfferB(item_amounts)
 
-    #rest of the items:
+    # rest of the items:
     for key in item_amounts.keys():
-        if item_amounts[key]>=0:  
-            basket+=single_items[key]*item_amounts[key]
-
-
-
-
+        if item_amounts[key] >= 0:
+            basket += single_items[key] * item_amounts[key]
 
     return basket
 
-
-    
 
 """
 +------+-------+----------------+
@@ -118,4 +124,5 @@ def checkout(skus):
 | D    | 15    |                |
 +------+-------+----------------+
 """
+
 
--- test/solution_tests/CHK/test_chk_1.py
+++ test/solution_tests/CHK/test_chk_1.py
@@ -3,43 +3,47 @@ from requests import check_compatibility
 
 import unittest
 import sys
-sys.path.append('../../../lib/solutions/CHK')
 
-#from solutions.solutions_CHK import checkout_solution
+sys.path.append("../../../lib/solutions/CHK")
+
+# from solutions.solutions_CHK import checkout_solution
 import checkout_solution
 
+
 class TestCHK(unittest.TestCase):
-    """Test single item, no special offers
-    """
+    """Test single item, no special offers"""
+
     def test_chk1_single(self):
         assert checkout_solution.checkout("A") == 50
 
-
     """Test multiple of the same item without special offers
     """
+
     def test_chk1_multiple(self):
-        assert checkout_solution.checkout("AA")==100
+        assert checkout_solution.checkout("AA") == 100
 
     """Test multiple different items without special offer
     """
+
     def test_chk1_mixed(self):
-        assert checkout_solution.checkout("ABC")==100
+        assert checkout_solution.checkout("ABC") == 100
 
     """Test a special offer only buy for single offer
     """
+
     def test_chk1_special_simple(self):
-        assert checkout_solution.checkout("AAA")==130
+        assert checkout_solution.checkout("AAA") == 130
 
     """Test a case with multiple special offers and nothing else
     """
+
     def test_chk1_special_two_offers_multiprice(self):
-        assert checkout_solution.checkout("AAAAAAAA")==330
+        assert checkout_solution.checkout("AAAAAAAA") == 330
 
     """Test a mixed basket with special offers and single items
     """
+
     def test_chk1_special_mixed(self):
-        assert checkout_solution.checkout("AAABBABCD")==290
-
-
+        assert checkout_solution.checkout("AAABBABCD") == 290
 
 
