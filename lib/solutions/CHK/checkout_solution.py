1bdb860abc6383855aa685351762b7a51dcdee36
sbx_user1051 <sbx_user1051@169.254.60.101>
2022-06-07 18:42:28 +0000

    Tue Jun 07 18:42:28 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -4,6 +4,33 @@
 # skus = unicode string
 
 
+from signal import ITIMER_REAL
+
+
+def sanityCheck(skus):
+    #empty basket
+    if not skus:
+        return 0
+    #malformed token
+    if not skus.isupper():
+        return -1
+
+def collateItemAmounts(skus, single_items):
+    item_amounts={}
+
+    for item in skus:
+        #Not in the basket
+        if not item in single_items:
+            return -1
+
+        #Add item to the item amounts, or increment
+        if item in item_amounts:
+            item_amounts[item]=item_amounts[item]+1
+        else:
+            item_amounts[item]=1
+    return item_amounts
+
+
 def checkout(skus):
     #First, we sum all possible amounts of items. Then, if
     #A special offer for an item exists, we try to fit that first. If not, then go with the normal price table
@@ -17,40 +44,18 @@ def checkout(skus):
 
     #Formulate a hashmap for single items, and another for special offers
     single_items={"A":50,"B":30,"C":20,"D":15}
-    #empty basket
-    if not skus:
-        return 0
-    #First, check if skus only includes uppercase letters
-    if not skus.isupper():
-        return -1
-
 
     #We do all non-special offer tests first.
     #Testing works now. Now to start with the first cases: iterate all the items, and
     #Add their prices into the basket.
     #If we find an item that is not in basket, return -1
 
-
-
     #Now all the basic cases are there.
     #In order to utilize the  special offers, we collect amounts of items to another map
 
 
 
-    item_amounts={}
-
-    for item in skus:
-        #Not in the basket
-        if not item in single_items:
-            return -1
-
-        #Add item to the item amounts, or increment
-        if item in item_amounts:
-            item_amounts[item]=item_amounts[item]+1
-        else:
-            item_amounts[item]=1
-
-
+    item_amounts=collateItemAmounts(skus,single_items)
     basket=0
     #First, we try to fit the special offers in:
     #If the amount of items is geq 3, recude
@@ -100,6 +105,7 @@ def checkout(skus):
 | D    | 15    |                |
 +------+-------+----------------+
 """
+
 
 
 
