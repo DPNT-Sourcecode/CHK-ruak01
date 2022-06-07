c49d73ad56c484335acbd638eb29474abc259153
sbx_user1051 <sbx_user1051@169.254.60.101>
2022-06-07 17:41:53 +0000

    Tue Jun 07 17:41:53 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,4 +3,7 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    raise NotImplementedError()
+    #First, we sum all possible amounts of items. Then, if
+    #A special offer for an item exists, we try to fit that first. If not, then go with the normal price table
+    
+
